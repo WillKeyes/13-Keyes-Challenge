d6144128a0316cc87af427aed8e4065dd1ec2f12
Will Keyes <willkeyes01@gmail.com>
2024-04-07 22:09:58 -0600

    Still trying to figure out why i cant do a run seed

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -1,5 +1,10 @@
--- DROP DATABASE
+ -- DROP DATABASE
 DROP DATABASE IF EXISTS ecommerce_db;
+DROP TABLE IF EXISTS `ProductTag`;
+DROP TABLE IF EXISTS `Product`;
+DROP TABLE IF EXISTS `Tag`;
+DROP TABLE IF EXISTS `Category`;
+
 
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
@@ -12,6 +17,17 @@ CREATE TABLE Category (
     category_name VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
+
+-- Create 'ProductTag' table
+CREATE TABLE ProductTag (
+    id INT NOT NULL AUTO_INCREMENT,
+    product_id INT,
+    tag_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (product_id) REFERENCES Product(id) ON DELETE CASCADE,
+    FOREIGN KEY (tag_id) REFERENCES Tag(id) ON DELETE CASCADE
+);
+
 
 -- Create 'Product' table
 CREATE TABLE Product (
@@ -31,12 +47,3 @@ CREATE TABLE Tag (
     PRIMARY KEY (id)
 );
 
--- Create 'ProductTag' table
-CREATE TABLE ProductTag (
-    id INT NOT NULL AUTO_INCREMENT,
-    product_id INT,
-    tag_id INT,
-    PRIMARY KEY (id),
-    FOREIGN KEY (product_id) REFERENCES Product(id) ON DELETE CASCADE,
-    FOREIGN KEY (tag_id) REFERENCES Tag(id) ON DELETE CASCADE
-);
--- Develop/server.js
+++ Develop/server.js
@@ -16,3 +16,4 @@ sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
 });
 
+// I keep running into this error and idk why

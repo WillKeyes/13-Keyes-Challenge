fc04c5f0a61a10964729bffdb00043c15d9c96fc
Will Keyes <willkeyes01@gmail.com>
2024-05-04 16:58:06 -0600

    Just making sure everything is up to date

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -7,16 +7,17 @@ class Category extends Model {}
 Category.init(
   {
     // define columns this is good
-    category_name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true
     },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    
   },
   {
     sequelize,
